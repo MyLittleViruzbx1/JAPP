389ca165f883a32849342441884b6f422a19c60c
MyLittle <mylittleviruz@outlook.es>
2021-09-23 00:24:40 -0600

    agregando mas contenido sw

--- sw.js
+++ sw.js
@@ -1,5 +1,8 @@
 const CACHE = 'JAPP-v1';
 
 self.addEventListener('install',e =>{
-    /*Cuando se instala el */
+    /*Cuando se instala el SW el cache de lo mas importante*/
+    const cacheProm = caches.open( CACHE ).then(cache=>{
+        
+    })
 });
